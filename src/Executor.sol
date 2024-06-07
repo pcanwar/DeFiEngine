51334def231fdf3bba1544f11a8700445d4356da
pcanwar <18260986+pcanwar@users.noreply.github.com>
2024-06-07 19:40:10 -0400

    implemented lowlevel transfer erc20 standard

---
+++ src/Executor.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: NONE
+pragma solidity ^0.8.25;
+
+import "./Constant.sol";
+import { IExecutor } from "./IExecutor.sol";
+abstract contract Executor is IExecutor {
+    constructor() {
+    }
+    function _safeTransferFrom(address token, address from, address to, uint256 amount) internal {
+    bool success;
+    assembly {
+        let ptr := mload(0x40)
+        mstore(ptr, ERC20_transferFrom_selector)
+        mstore(add(ptr, 0x04), from)
+        mstore(add(ptr, 0x24), to)
+        mstore(add(ptr, 0x44), amount)
+        success := call(gas(), token, 0, ptr, ERC20_transferFrom_size, 0, 0)
+    }
+    // require(success, "transfer failed");
+    if (!success) {
+        revert TransferFailed();
+    }
+}
+
+}
--- src/StakingRewards.sol
+++ src/StakingRewards.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: None
 pragma solidity ^0.8.25;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import {StakeInfo} from "./Struct.sol";
-
+import "./Executor.sol";
 
 
 contract StakingRewards is ReentrancyGuard, Ownable {
-    using SafeERC20 for IERC20;
+    // using SafeERC20 for IERC20;
+
+    constructor(
+    ) Ownable(msg.sender) {} 
 
 
 }
