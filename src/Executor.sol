9bf7c6746d596023d8726052da83931fad2ebc71
pcanwar <18260986+pcanwar@users.noreply.github.com>
2024-06-07 22:46:48 -0400

    excutor transfer selector

--- src/Executor.sol
+++ src/Executor.sol
@@ -7,19 +7,33 @@ abstract contract Executor is IExecutor {
     constructor() {
     }
     function _safeTransferFrom(address token, address from, address to, uint256 amount) internal {
-    bool success;
-    assembly {
-        let ptr := mload(0x40)
-        mstore(ptr, ERC20_transferFrom_selector)
-        mstore(add(ptr, 0x04), from)
-        mstore(add(ptr, 0x24), to)
-        mstore(add(ptr, 0x44), amount)
-        success := call(gas(), token, 0, ptr, ERC20_transferFrom_size, 0, 0)
+        bool success;
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, ERC20_transferFrom_selector)
+            mstore(add(ptr, 0x04), from)
+            mstore(add(ptr, 0x24), to)
+            mstore(add(ptr, 0x44), amount)
+            success := call(gas(), token, 0, ptr, ERC20_transferFrom_size, 0, 0)
+        }
+        // require(success, "transfer failed");
+        if (!success) {
+            revert TransferFailed();
+        }
     }
-    // require(success, "transfer failed");
-    if (!success) {
-        revert TransferFailed();
+
+    function _safeTransfer(address token, address to, uint256 amount) internal {
+        bool success;
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, ERC20_transfer_selector)
+            mstore(add(ptr, 0x04), to)
+            mstore(add(ptr, 0x24), amount)
+            success := call(gas(), token, 0, ptr, ERC20_transfer_size, 0, 0)
+        }
+        if (!success) {
+            revert TransferFailed();
+        }
     }
-}
 
 }
